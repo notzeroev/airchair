a932ee55e664a7fdab8e1a42d4bc046f08c245a9
notzeroev <nathanaaron117gmail.com>
2025-06-13 18:20:30 +1000

    updated initial page to be rendered on client side

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,17 +1,13 @@
-import { api, HydrateClient } from "@/trpc/server";
+"use client";
 
-export default async function Home() {
-  void api.post.getLatest.prefetch();
-
+export default function Home() {
   return (
-    <HydrateClient>
-      <main className="flex flex-col items-center justify-center bg-background">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-2xl font-bold tracking-tight">
-            Dashboard under construction...
-          </h1>
-        </div>
-      </main>
-    </HydrateClient>
+    <main className="flex flex-col items-center justify-center bg-background">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 className="text-2xl font-bold tracking-tight">
+          Dashboard under construction...
+        </h1>
+      </div>
+    </main>
   );
 }
